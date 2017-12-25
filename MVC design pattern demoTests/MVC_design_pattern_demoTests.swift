fb811b3fa5e6b3c488d046ae8c2aa8c6675accc9
Neha Methiwala <neham@mobifyi.com>
2017-12-25 15:31:08 +0530

    Initial Commit

---
+++ MVC design pattern demoTests/MVC_design_pattern_demoTests.swift
@@ -0,0 +1,36 @@
+//
+//  MVC_design_pattern_demoTests.swift
+//  MVC design pattern demoTests
+//
+//  Created by Neha Methiwala on 25/12/17.
+//  Copyright © 2017 rahulSharma. All rights reserved.
+//
+
+import XCTest
+@testable import MVC_design_pattern_demo
+
+class MVC_design_pattern_demoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
